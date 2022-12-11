261362c957a49a974943562f726353b50d467db8
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-11 14:23:28 +0100

    added rough employeeDao implementation

---
+++ src/main/java/ba/unsa/etf/rpr/dao/EmployeeDAO.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Employee;
+
+public interface EmployeeDAO extends Dao<Employee>{
+
+}
---
+++ src/main/java/ba/unsa/etf/rpr/domain/Employee.java
@@ -0,0 +1,66 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(getFirstName(), employee.getFirstName()) && Objects.equals(getLastName(), employee.getLastName()) && Objects.equals(getUsername(), employee.getUsername()) && Objects.equals(getPassword(), employee.getPassword());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName(), getUsername(), getPassword());
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String username;
+    private String password;
+}
