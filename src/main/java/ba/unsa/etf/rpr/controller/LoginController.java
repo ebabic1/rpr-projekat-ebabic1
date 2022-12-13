a50a5b8e43cf1f9d0d2a3220d6b36c17e7c43192
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-13 23:22:20 +0100

    login page changes

--- src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
@@ -1,16 +1,52 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class LoginController {
 
     public TextField passwordField;
     public TextField usernameField;
     public Button loginButton;
-
-    public void loginClicked(MouseEvent mouseEvent) {
-
+    @FXML
+    public void initialize(){
+        usernameField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String o, String n) {
+                if (usernameField.getText().trim().isEmpty()){
+                    usernameField.getStyleClass().removeAll("poljeIspravno");
+                    usernameField.getStyleClass().add("poljeNijeIspravno");
+                }
+                else{
+                    usernameField.getStyleClass().removeAll("poljeNijeIspravno");
+                    usernameField.getStyleClass().add("poljeIspravno");
+                }
+            }
+        });
+    }
+    public void loginClicked(MouseEvent mouseEvent) throws IOException {
+        if (usernameField.getText().trim().isEmpty() || passwordField.getText().trim().isEmpty()){
+            return;
+        }
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/dashboard.fxml"));
+        Parent root = loader.load();
+        DashboardController dashboardController = loader.getController();
+        dashboardController.labelIme.setText(usernameField.getText());
+        stage.setTitle("Dashboard");
+        stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        stage.showAndWait();
     }
 }
