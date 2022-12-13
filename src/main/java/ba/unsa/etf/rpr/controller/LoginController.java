813db8d7c01f4189cc8198542a14d1ffb5637d47
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-13 22:26:37 +0100

    login changes

--- src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
@@ -1,9 +1,16 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 public class LoginController {
 
+    public TextField passwordField;
+    public TextField usernameField;
+    public Button loginButton;
+
     public void loginClicked(MouseEvent mouseEvent) {
+
     }
 }
