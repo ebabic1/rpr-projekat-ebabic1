24035e46957b06cb6368d5167ab86c5f55606f4e
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-13 22:14:53 +0100

    room bean added columns

--- src/main/java/ba/unsa/etf/rpr/domain/Room.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Room.java
@@ -9,26 +9,45 @@ import java.util.Objects;
 public class Room {
     private int roomId;
     private int maxPersons;
+    private String description;
+    private int available;
 
     @Override
     public String toString() {
-        return "Rooms{" +
+        return "Room{" +
                 "roomId=" + roomId +
                 ", maxPersons=" + maxPersons +
+                ", description='" + description + '\'' +
+                ", available=" + available +
                 '}';
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Room)) return false;
-        Room room = (Room) o;
-        return getRoomId() == room.getRoomId() && getMaxPersons() == room.getMaxPersons();
+        if (!(o instanceof Room room)) return false;
+        return getRoomId() == room.getRoomId() && getMaxPersons() == room.getMaxPersons() && getAvailable() == room.getAvailable() && Objects.equals(getDescription(), room.getDescription());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getRoomId(), getMaxPersons());
+        return Objects.hash(getRoomId(), getMaxPersons(), getDescription(), getAvailable());
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
     }
 
     public int getRoomId() {
