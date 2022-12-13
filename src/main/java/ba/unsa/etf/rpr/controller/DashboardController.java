82a45d35839e29d4c676dfa0a2bfe7e40ecfb104
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-13 23:45:39 +0100

    dashboar navigation

--- src/main/java/ba/unsa/etf/rpr/controller/DashboardController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/DashboardController.java
@@ -1,9 +1,16 @@
 package ba.unsa.etf.rpr.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import java.io.IOException;
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class DashboardController {
     @FXML
@@ -12,6 +19,15 @@ public class DashboardController {
     public Button logoutButton;
 
 
-    public void logoutClicked(MouseEvent mouseEvent) {
+    public void logoutClicked(MouseEvent mouseEvent) throws IOException {
+        Node n = (Node) mouseEvent.getSource();
+        Stage s1 = (Stage) n.getScene().getWindow();
+        s1.close();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Hello!");
+        stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        stage.show();
     }
 }
