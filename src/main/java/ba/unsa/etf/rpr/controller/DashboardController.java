18d8b61cd76790072f03b523abbe00f14a1087ac
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-13 22:26:57 +0100

    dashboard added controller

---
+++ src/main/java/ba/unsa/etf/rpr/controller/DashboardController.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+
+public class DashboardController {
+    @FXML
+    private Label labelIme;
+    @FXML
+    private Button logoutButton;
+
+
+    public void logoutClicked(MouseEvent mouseEvent) {
+    }
+}
