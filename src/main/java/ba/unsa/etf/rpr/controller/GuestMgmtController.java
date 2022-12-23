86b9d521b645660c5a921ad129796061e2137f69
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-23 01:14:25 +0100

    add guest management window controller

--- src/main/java/ba/unsa/etf/rpr/controller/GuestMgmtController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/GuestMgmtController.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class GuestMgmtController {
+    public void addOpenWindow(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addguest.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Add a guest");
+        stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+    }
 }
