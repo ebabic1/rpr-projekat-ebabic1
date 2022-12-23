2f9864bad4bebdd6fd5d2bb6e1c8f32a843f3314
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-23 01:07:48 +0100

    add login functionalities

--- src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/LoginController.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.controller;
 
+import ba.unsa.etf.rpr.dao.UserDaoSQLImplementation;
+import ba.unsa.etf.rpr.domain.User;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
@@ -8,6 +10,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -18,7 +21,7 @@ import java.io.IOException;
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class LoginController {
-
+    public Label wrongPasswordLabel;
     public PasswordField passwordField;
     public TextField usernameField;
     public Button loginButton;
@@ -57,6 +60,18 @@ public class LoginController {
         if (usernameField.getText().trim().isEmpty() || passwordField.getText().trim().isEmpty()){
             return;
         }
+        UserDaoSQLImplementation uDao = new UserDaoSQLImplementation();
+        User u = uDao.getByUsername(passwordField.getText());
+        if (u == null){
+            System.out.println("Korisnik nije nađen");
+            return;
+        }
+        if( !u.getPassword().equals(passwordField.getText()) || u.getAdmin() != 1){
+            wrongPasswordLabel.setVisible(true);
+            passwordField.getStyleClass().removeAll("poljeIspravno");
+            passwordField.getStyleClass().add("poljeIspravno");
+            return;
+        }
         Node n = (Node) mouseEvent.getSource();
         Stage s1 = (Stage) n.getScene().getWindow();
         s1.close();
@@ -65,8 +80,11 @@ public class LoginController {
         Parent root = loader.load();
         DashboardController dashboardController = loader.getController();
         dashboardController.labelIme.setText(usernameField.getText());
+        dashboardController.mainPane.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
         stage.setTitle("Dashboard");
         stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        stage.setMinWidth(500);
+        stage.setMinHeight(550);
         stage.showAndWait();
     }
 }
