ad27608b9ba6e20c4d9a14f96174d88fd2fe52f3
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-27 22:42:36 +0100

    add usermodel

---
+++ src/main/java/ba/unsa/etf/rpr/model/UserModel.java
@@ -0,0 +1,135 @@
+package ba.unsa.etf.rpr.model;
+
+import ba.unsa.etf.rpr.domain.User;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+/**
+ * Model class for 2 way data binding with add/update form
+ *
+ * @autor Eldar Babić
+ */
+public class UserModel {
+    public SimpleStringProperty firstNameField;
+    public SimpleStringProperty  lastNameField;
+    public SimpleStringProperty  cityComboBox;
+    public SimpleStringProperty  countryComboBox;
+    public SimpleStringProperty  emailField;
+    public SimpleStringProperty  usernameField;
+    public void fromUser(User u) {
+        this.firstNameField.set(u.getFirstName());
+        this.lastNameField.set(u.getLastName());
+        this.cityComboBox.set(u.getCity());
+        this.countryComboBox.set(u.getCountry());
+        this.emailField.set(u.getEmail());
+        this.usernameField.set(u.getUsername());
+    }
+    public void toUser(){
+        User u = new User();
+        u.setFirstName(this.firstNameField.getValue());
+        u.setLastName(this.lastNameField.getValue());
+        u.setCity(this.cityComboBox.getValue());
+        u.setCountry(this.countryComboBox.getValue());
+        u.setEmail(this.emailField.getValue());
+        u.setUsername(this.usernameField.getValue());
+    }
+    public String getFirstNameField() {
+        return firstNameField.get();
+    }
+
+    public SimpleStringProperty firstNameFieldProperty() {
+        return firstNameField;
+    }
+
+    public void setFirstNameField(String firstNameField) {
+        this.firstNameField.set(firstNameField);
+    }
+
+    public String getLastNameField() {
+        return lastNameField.get();
+    }
+
+    public SimpleStringProperty lastNameFieldProperty() {
+        return lastNameField;
+    }
+
+    public void setLastNameField(String lastNameField) {
+        this.lastNameField.set(lastNameField);
+    }
+
+    public String getCityComboBox() {
+        return cityComboBox.get();
+    }
+
+    public SimpleStringProperty cityComboBoxProperty() {
+        return cityComboBox;
+    }
+
+    public void setCityComboBox(String cityComboBox) {
+        this.cityComboBox.set(cityComboBox);
+    }
+
+    public String getCountryComboBox() {
+        return countryComboBox.get();
+    }
+
+    public SimpleStringProperty countryComboBoxProperty() {
+        return countryComboBox;
+    }
+
+    public void setCountryComboBox(String countryComboBox) {
+        this.countryComboBox.set(countryComboBox);
+    }
+
+    public String getEmailField() {
+        return emailField.get();
+    }
+
+    public SimpleStringProperty emailFieldProperty() {
+        return emailField;
+    }
+
+    public void setEmailField(String emailField) {
+        this.emailField.set(emailField);
+    }
+
+    public String getUsernameField() {
+        return usernameField.get();
+    }
+
+    public SimpleStringProperty usernameFieldProperty() {
+        return usernameField;
+    }
+
+    public void setUsernameField(String usernameField) {
+        this.usernameField.set(usernameField);
+    }
+
+    public String getPasswordField() {
+        return passwordField.get();
+    }
+
+    public SimpleStringProperty passwordFieldProperty() {
+        return passwordField;
+    }
+
+    public void setPasswordField(String passwordField) {
+        this.passwordField.set(passwordField);
+    }
+
+    public String getPhoneField() {
+        return phoneField.get();
+    }
+
+    public SimpleStringProperty phoneFieldProperty() {
+        return phoneField;
+    }
+
+    public void setPhoneField(String phoneField) {
+        this.phoneField.set(phoneField);
+    }
+
+    public SimpleStringProperty  passwordField;
+    public SimpleStringProperty  phoneField;
+}
