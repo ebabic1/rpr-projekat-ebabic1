b07f0f9682b9cef1b6687f3c26cf7c1d250260e6
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-27 21:35:11 +0100

    guestmgmt form changes

--- src/main/java/ba/unsa/etf/rpr/controller/GuestMgmtController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/GuestMgmtController.java
@@ -1,9 +1,15 @@
 package ba.unsa.etf.rpr.controller;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.User;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,13 +17,47 @@ import java.io.IOException;
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class GuestMgmtController {
+    public Button editButton;
+    public Button addButton;
+    public TextField searchTextField;
+    public Button deleteButton;
+    public TableColumn<User,String> idColumn;
+    public TableColumn<User,String> firstNameColumn;
+    public TableColumn<User,String> lastNameColumn;
+    public TableColumn<User,String> cityColumn;
+    public TableColumn<User,String> countryColumn;
+    public TableColumn<User,String> emailColumn;
+    public TableColumn<User,String> phoneColumn;
+    public TableView guestsTabela;
+
+    @FXML
+    public void initialize() {
+        idColumn.setCellValueFactory(new PropertyValueFactory<User,String>("id"));
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<User,String>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<User,String>("lastName"));
+        cityColumn.setCellValueFactory(new PropertyValueFactory<User,String>("city"));
+        countryColumn.setCellValueFactory(new PropertyValueFactory<User,String>("country"));
+        emailColumn.setCellValueFactory(new PropertyValueFactory<User,String>("email"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<User,String>("phone"));
+        refreshGuests();
+    }
+    public void createUpdateGuest(int id){
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/adduser.fxml"));
+            Parent root = loader.load();
+            stage.setTitle("Add a guest");
+            stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.show();
+        }catch (Exception e) {
+            new Alert(Alert.AlertType.NONE,e.getMessage(),ButtonType.OK).show();
+        }
+    }
     public void addOpenWindow(ActionEvent actionEvent) throws IOException {
-        Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addguest.fxml"));
-        Parent root = loader.load();
-        stage.setTitle("Add a guest");
-        stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
-        stage.setResizable(false);
-        stage.show();
+
+    }
+    private void refreshGuests(){
+        guestsTabela.setItems(FXCollections.observableList(DaoFactory.userDao().getAll()));
     }
 }
