96b6ceae6ddb7154113c2f8866a7772bffb3ebe4
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-27 22:42:20 +0100

    adduser changes

---
+++ src/main/java/ba/unsa/etf/rpr/controller/AddUpdateUserController.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.controller;
+
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class AddUpdateUserController {
+    public TextField firstNameField;
+    public TextField lastNameField;
+    public ComboBox cityComboBox;
+    public ComboBox countryComboBox;
+    public TextField emailField;
+    public TextField usernameField;
+    public TextField passwordField;
+    public TextField phoneField;
+}
