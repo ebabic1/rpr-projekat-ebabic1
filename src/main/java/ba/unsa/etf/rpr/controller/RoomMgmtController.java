cda0ca32be3ad5fcec2edfa2ba651615af7c4def
ebabic1 <ebabic1@etf.unsa.ba>
2022-12-31 00:08:07 +0100

    roommgmt changes

--- src/main/java/ba/unsa/etf/rpr/controller/RoomMgmtController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/RoomMgmtController.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class RoomMgmtController {
+    public Button editButton;
+    public Button addButton;
+    public Button deleteButton;
+    public TextField searchTextField;
+    public TableView roomsTable;
+    public TableColumn idColumn;
+    public TableColumn priceColumn;
+    public TableColumn maxPersonsColumn;
+    public TableColumn descriptionColumn;
+    public TableColumn availableColumn;
+
+    public void editButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void addButtonPressed(ActionEvent actionEvent) {
+    }
+
+    public void deleteButtonPressed(ActionEvent actionEvent) {
+    }
 }
