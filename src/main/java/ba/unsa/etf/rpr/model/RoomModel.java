37193e3bfcc4f98b8e64071d6f7a8326fde34392
ebabic1 <ebabic1@etf.unsa.ba>
2023-01-01 15:54:38 +0100

    add RoomModel class

---
+++ src/main/java/ba/unsa/etf/rpr/model/RoomModel.java
@@ -0,0 +1,64 @@
+package ba.unsa.etf.rpr.model;
+
+import ba.unsa.etf.rpr.domain.Room;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Model class for 2 way data binding with add/update form
+ * @author Eldar Babić
+ */
+public class RoomModel {
+    public SimpleDoubleProperty priceField = new SimpleDoubleProperty();
+    public SimpleStringProperty descriptionField = new SimpleStringProperty();
+    public SimpleIntegerProperty maxPersonsField = new SimpleIntegerProperty();
+    public void fromRoom(Room r) {
+        this.priceField.set(r.getPrice());
+        this.descriptionField.set(r.getDescription());
+        this.maxPersonsField.set(r.getMaxPersons());
+    }
+    public Room toRoom() {
+        Room r = new Room();
+        r.setPrice(this.priceField.getValue());
+        r.setDescription(this.descriptionField.getValue());
+        r.setMaxPersons(this.maxPersonsField.getValue());
+        return r;
+    }
+
+    public double getPriceField() {
+        return priceField.get();
+    }
+
+    public SimpleDoubleProperty priceFieldProperty() {
+        return priceField;
+    }
+
+    public void setPriceField(double priceField) {
+        this.priceField.set(priceField);
+    }
+
+    public String getDescriptionField() {
+        return descriptionField.get();
+    }
+
+    public SimpleStringProperty descriptionFieldProperty() {
+        return descriptionField;
+    }
+
+    public void setDescriptionField(String descriptionField) {
+        this.descriptionField.set(descriptionField);
+    }
+
+    public int getMaxPersonsField() {
+        return maxPersonsField.get();
+    }
+
+    public SimpleIntegerProperty maxPersonsFieldProperty() {
+        return maxPersonsField;
+    }
+
+    public void setMaxPersonsField(int maxPersonsField) {
+        this.maxPersonsField.set(maxPersonsField);
+    }
+}
