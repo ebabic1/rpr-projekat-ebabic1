66087443f562fa5c8e35222683d2f0cf85137d1b
ebabic1 <ebabic1@etf.unsa.ba>
2023-01-01 17:55:14 +0100

    AddUpdateReservationController changes

--- src/main/java/ba/unsa/etf/rpr/controller/AddUpdateReservationController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/AddUpdateReservationController.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class AddUpdateReservationController {
+    public Label firstNameLabel;
+    public Label lastNameLabel;
+    public Label cityLabel;
+    public Label countryLabel;
+    public Label emailLabel;
+    public Label phoneLabel;
+    public Button okButton;
+
+    public void okPressed(ActionEvent actionEvent) {
+    }
+
+    public void cancelPressed(ActionEvent actionEvent) {
+    }
 }
