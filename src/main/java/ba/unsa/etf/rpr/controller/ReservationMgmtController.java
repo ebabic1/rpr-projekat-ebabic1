400e89a750c6d0b830c3f75a0dccb05078dfbec4
ebabic1 <ebabic1@etf.unsa.ba>
2023-01-03 15:46:38 +0100

    ReservationMgmtController changes to match Reservation bean changes

--- src/main/java/ba/unsa/etf/rpr/controller/ReservationMgmtController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/ReservationMgmtController.java
@@ -1,16 +1,22 @@
 package ba.unsa.etf.rpr.controller;
 
+import ba.unsa.etf.rpr.business.ReservationManager;
 import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Reservation;
+import ba.unsa.etf.rpr.exceptions.ReservationException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import java.sql.SQLException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class ReservationMgmtController {
     public Button editButton;
@@ -24,6 +30,28 @@ public class ReservationMgmtController {
     public TableColumn additionalInfoColumn;
     public TableColumn userIdColumn;
     public TableColumn roomIdColumn;
+    private ReservationManager reservationManager = new ReservationManager();
+    private void addupdateScene(Integer rId, Integer uId){
+        try {
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/addreservation.fxml"));
+            loader.setController(new AddUpdateReservationController(rId,uId));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.setResizable(false);
+            stage.setOnHiding(event -> {
+                refreshReservations();
+            } );
+            if(rId != null) {
+                stage.setTitle("Edit a reservation");
+            }
+            else stage.setTitle("Add a reservation");
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     private void refreshReservations() {
         try {
             reservationsTable.setItems(FXCollections.observableList(DaoFactory.reservationDao().getAll()));
@@ -37,18 +65,30 @@ public class ReservationMgmtController {
         arrivalDateColumn.setCellValueFactory(new PropertyValueFactory<>("arrivalDate"));
         leaveDateColumn.setCellValueFactory(new PropertyValueFactory<>("leaveDate"));
         additionalInfoColumn.setCellValueFactory(new PropertyValueFactory<>("additionalInfo"));
-        roomIdColumn.setCellValueFactory(new PropertyValueFactory<>("roomId"));
-        userIdColumn.setCellValueFactory(new PropertyValueFactory<>("userId"));
         refreshReservations();
 
     }
     public void editButtonPressed(ActionEvent actionEvent) {
-
+        Reservation selectedReservation = (Reservation) reservationsTable.getSelectionModel().getSelectedItem();
+        Integer rId = selectedReservation.getId();
+        Integer uId = selectedReservation.getUser().getId();
+        if(rId != null){
+            addupdateScene(rId,uId);
+        }
     }
 
     public void addButtonPressed(ActionEvent actionEvent) {
+
     }
 
     public void deleteButtonPressed(ActionEvent actionEvent) {
+        Reservation selectedReservation = (Reservation) reservationsTable.getSelectionModel().getSelectedItem();
+        Integer rId = selectedReservation.getId();
+        try {
+            reservationManager.delete(rId);
+            refreshReservations();
+        } catch (ReservationException e) {
+            new Alert(Alert.AlertType.ERROR,e.getMessage(),ButtonType.OK).show();
+        }
     }
 }
