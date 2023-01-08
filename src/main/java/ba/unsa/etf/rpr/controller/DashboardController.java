3cab6d2dcf97295ed9123251c424d648e307f3d2
ebabic1 <ebabic1@etf.unsa.ba>
2023-01-08 23:49:45 +0100

    Dashboard changes

--- src/main/java/ba/unsa/etf/rpr/controller/DashboardController.java
+++ src/main/java/ba/unsa/etf/rpr/controller/DashboardController.java
@@ -1,6 +1,5 @@
 package ba.unsa.etf.rpr.controller;
 
-import ba.unsa.etf.rpr.dao.DaoFactory;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,17 +13,10 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.sql.SQLException;
-
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class DashboardController {
-    @FXML
     public GridPane mainPane;
-    @FXML
-    public Label totalGuestsLabel;
-    @FXML
-    public Label availableRoomsLabel;
     @FXML
     private Button dashboardButton;
 
@@ -48,16 +40,6 @@ public class DashboardController {
 
     @FXML
     private BorderPane mainBorderPane;
-    @FXML
-    public void initialize(){
-        try {
-            totalGuestsLabel.setText(String.valueOf(DaoFactory.userDao().getAll().size()));
-            availableRoomsLabel.setText(String.valueOf(DaoFactory.roomDao().getAll().size() - DaoFactory.reservationDao().getAll().size()));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
 
     public void logoutClicked(MouseEvent mouseEvent) throws IOException {
         Node n = (Node) mouseEvent.getSource();
@@ -87,15 +69,5 @@ public class DashboardController {
         GridPane gridPane = FXMLLoader.load(getClass().getResource("/fxml/reservationmgmt.fxml"));
         gridPane.setMaxSize(Double.MAX_VALUE,Double.MAX_VALUE);
         mainBorderPane.setCenter(gridPane);
-    }
-
-    public void dBrdPressed(ActionEvent actionEvent) {
-        try {
-            totalGuestsLabel.setText(String.valueOf(DaoFactory.userDao().getAll().size()));
-            availableRoomsLabel.setText(String.valueOf(DaoFactory.roomDao().getAll().size() - DaoFactory.reservationDao().getAll().size()));
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        mainBorderPane.setCenter(mainPane);
     }
 }
