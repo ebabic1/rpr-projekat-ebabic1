ef00a7085cee01ac9e015c0bda1f683e6bc0fc84
ebabic1 <ebabic1@etf.unsa.ba>
2023-01-08 17:57:26 +0100

    add GuestDashboardController

---
+++ src/main/java/ba/unsa/etf/rpr/controller/GuestDashboardController.java
@@ -0,0 +1,37 @@
+package ba.unsa.etf.rpr.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class GuestDashboardController {
+    @FXML
+    public Label nameLabel;
+    @FXML
+    public Label roomNoLabel;
+    public Button logoutButton;
+
+
+    public void logoutClicked(ActionEvent actionEvent) throws IOException {
+        Node n = (Node) actionEvent.getSource();
+        Stage s1 = (Stage) n.getScene().getWindow();
+        s1.close();
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Hello!");
+        stage.setScene(new Scene(root,USE_COMPUTED_SIZE,USE_COMPUTED_SIZE));
+        stage.show();
+    }
+}
