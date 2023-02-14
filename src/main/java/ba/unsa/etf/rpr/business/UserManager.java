60a1bc63e2145a8ecb385bb5096346b27fe08b16
ebabic1 <ebabic1@etf.unsa.ba>
2023-02-14 11:34:21 +0100

    add javadocs

--- src/main/java/ba/unsa/etf/rpr/business/UserManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/UserManager.java
@@ -9,12 +9,22 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class UserManager {
-    public void validate(String name) throws UserException{
-        if (name == null ||name.trim().length() == 0) {
+    /**
+     * Validates user's name
+     * @param u
+     * @throws UserException
+     */
+    public void validate(User u) throws UserException{
+        if (u.getFirstName() == null || u.getFirstName().trim().length() == 0 || u.getLastName().trim().length() == 0) {
             throw new UserException("Name must be at least one character");
         }
     }
 
+    /**
+     * Deletes user given by parameter from database
+     * @param id
+     * @throws UserException
+     */
     public void delete(int id) throws UserException {
         try {
             DaoFactory.userDao().delete(id);
@@ -22,14 +32,29 @@ public class UserManager {
             throw new UserException(e.getMessage());
         }
     }
+
+    /**
+     * Adds user given by parameter to database
+     * @param u
+     * @return
+     * @throws UserException
+     */
     public User add(User u) throws UserException {
         try {
+            validate(u);
             DaoFactory.userDao().add(u);
         } catch (SQLException e) {
             throw new UserException(e.getMessage());
         }
         return u;
     }
+
+    /**
+     * Updates user given by parameter
+     * @param u
+     * @return
+     * @throws UserException
+     */
     public User update(User u) throws UserException{
         try {
             return DaoFactory.userDao().update(u);
@@ -37,6 +62,12 @@ public class UserManager {
             throw new UserException(e.getMessage());
         }
     }
+
+    /**
+     * Gets all users from database
+     * @return List of users
+     * @throws UserException
+     */
     public List<User> getAll() throws UserException {
         try {
             return DaoFactory.userDao().getAll();
@@ -44,6 +75,13 @@ public class UserManager {
             throw new UserException(e.getMessage());
         }
     }
+
+    /**
+     * Gets user from database by ID
+     * @param uId
+     * @return
+     * @throws UserException
+     */
     public User getById(int uId) throws UserException {
         try {
             return DaoFactory.userDao().getById(uId);
